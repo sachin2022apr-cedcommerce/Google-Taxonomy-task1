228febb376ceae5635a93e8deb0632678dba29d1
sachin2022apr-cedcommerce <sachin2022apr@cedcoss.com>
2022-12-12 14:05:04 +0530

    taxonomy task

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,31 @@
-import { Card, FlexChild, FlexLayout, Select } from '@cedcommerce/ounce-ui';
+import { FlexChild, FlexLayout, Select, Toast, ToastWrapper } from '@cedcommerce/ounce-ui';
 import React, { useEffect, useState } from 'react'
 import { useFetch } from '../fetchHook/FetchHook'
 export default function Home() {
   const [amazonCategory, setAmazonCategory] = useState([]);
-  const [selectedArray, setSelectedArry] = useState([]);
+  const [selectedArray, setSelectedArray] = useState([]);
   const [options, setOptions] = useState([])
+  const [toastActive, setToastActive] = useState(false)
   const { fetchData } = useFetch();
   useEffect(() => {
     fetchData()
-      .then(temp => {
+      .then(result => {
         const data = []
-        temp.split("\n").forEach((item, index) => {
-          data.push(item.split(">"))
+        result.split("\n").forEach((item, index) => {
+          data.push(item.split(" > "))
         });
         setAmazonCategory([...data])
         const tempOptions = []
+
         data.forEach((item) => {
-          tempOptions.push((item[selectedArray.length]).replace(/^\s+|\s+$/gm, ''))
+          tempOptions.push(item[0])
         })
-        let  t1 = [...getUnique(tempOptions)]
-        console.log(t1);
+        let t1 = [...getUnique(tempOptions)]
+        let tempData = [];
         t1.forEach((item, index) => {
-          console.log(item);
+          tempData.push({ label: item, value: item })
         })
+        setOptions([tempData])
       })
   }, [])
 
@@ -30,41 +33,75 @@ export default function Home() {
     let uniqueArr = [...new Set(array)];
     return uniqueArr
   }
+
   useEffect(() => {
-    console.log(options);
-  })
-  return (
-    <div>
-      <FlexLayout halign="center" spacing="loose">
-        <FlexChild desktopWidth="66" tabWidth="75" mobileWidth="80">
-          <Card
-            cardType="Bordered">
-            {options.map((item, index) => {
-              // return (<Select
-              //     dropDownheight={200}
-              //     onChange={function noRefCheck() { }}
-              //     options= {[item]}
-              //     placeholder="Select"
-              //     // popoverContainer="body"
-              //     thickness="thin"
-              //   />)
-              console.log([...item]);
-            })} 
-          </Card>
-        </FlexChild>
-      </FlexLayout>
-      {/* {options.map((item, index) => {
-        return (
-          <select key={`parent${index}`}>
-            {(item !== undefined) ? <>
-              {item.map((childItem, childIndex) => {
-                return <option>
-                  {childItem}
-                </option>
-              })}
-            </> : <></>}
-          </select>)
-      })} */}
-    </div>
-  )
+    let optionLength = selectedArray.length
+    const tempOptions = []
+    amazonCategory.forEach((item, index) => {
+      if (selectedArray[optionLength - 1].value === item[optionLength - 1] && item[optionLength] !== undefined) {
+        tempOptions.push(item[optionLength])
+      }
+    })
+    if (tempOptions.length) {
+      console.log(tempOptions)
+      let t1 = [...getUnique(tempOptions)]
+      let tempData = []
+      t1.forEach((item, index) => {
+        tempData.push({ label: item, value: item })
+      })
+      setOptions([...options, tempData])
+    } else {
+      if (options.length)
+        setToastActive(true)
+    }
+
+  }, [selectedArray])
+  console.log(amazonCategory);
+  const handleSelectValue = (value, index) => {
+    var optionSplice = [...options]
+    var selectedSplice = [...selectedArray]
+
+    if (selectedArray.length > index) {
+      optionSplice.splice(index + 1);
+      selectedSplice.splice(index + 1);
+      setOptions([...optionSplice])
+    }
+
+    selectedSplice[index] = {
+      label: value,
+      value: value
+    }
+    setSelectedArray([...selectedSplice])
+  }
+
+  const handleToast = () => {
+    setToastActive(!toastActive)
+  }
+  const toastMarkup = toastActive ? (<Toast
+    message="no more child"
+    onDismiss={() => handleToast()}
+    timeout={3000}
+    type="error"
+  />) : <></>
+
+  return (<>
+    <FlexLayout halign="center" spacing="tight">
+      {options.map((item, index) => {
+        return (<FlexChild desktopWidth="50" tabWidth="66" mobileWidth="80">
+          <Select
+            dropDownheight={500}
+            onChange={(value) => handleSelectValue(value, index)}
+            options={[...item]}
+            placeholder="Select"
+            thickness="thin"
+            value={selectedArray[index]?.value}
+          />
+        </FlexChild>)
+      })}
+
+    </FlexLayout>
+    <ToastWrapper>
+      {toastMarkup}
+    </ToastWrapper>
+  </>)
 }
--- src/fetchHook/FetchHook.js
+++ src/fetchHook/FetchHook.js
@@ -1,6 +1,5 @@
 export const useFetch = () => {
   async function fetchData(s) {
-    // console.log();
     const result = await fetch('./data.txt')
     return await result.text()
   }
