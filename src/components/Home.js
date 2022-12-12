74e5119413120111fc5cc03701f79f10d0b0abd7
sachin2022apr-cedcommerce <sachin2022apr@cedcoss.com>
2022-12-12 17:42:52 +0530

    taxonomy final

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,82 +7,61 @@ export default function Home() {
   const [options, setOptions] = useState([])
   const [toastActive, setToastActive] = useState(false)
   const { fetchData } = useFetch();
+
   useEffect(() => {
     fetchData()
       .then(result => {
-        const data = []
-        result.split("\n").forEach((item, index) => {
+        let data = []
+        result.split("\n").forEach((item) => {
           data.push(item.split(" > "))
-        });
+        })
         setAmazonCategory([...data])
-        const tempOptions = []
-
-        data.forEach((item) => {
-          tempOptions.push(item[0])
-        })
-        let t1 = [...getUnique(tempOptions)]
-        let tempData = [];
-        t1.forEach((item, index) => {
-          tempData.push({ label: item, value: item })
-        })
-        setOptions([tempData])
       })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  useEffect(() => {
+    let optionLength = selectedArray.length
+    let tempOptions = []
+    amazonCategory.forEach((item) => {
+      if (optionLength === 0)
+        tempOptions.push(item[optionLength])
+      else if (selectedArray[optionLength - 1].value === item[optionLength - 1] && item[optionLength] !== undefined)
+        tempOptions.push(item[optionLength])
+    })
+    if (tempOptions.length) {
+      let optionDataObject = []
+      let uniqueOptions = [...getUnique(tempOptions)]
+      uniqueOptions.forEach((item) => {
+        optionDataObject.push({ label: item, value: item })
+      })
+      if (!options.length)
+        setOptions([optionDataObject])
+      else
+        setOptions([...options, [...optionDataObject]])
+    }
+    else {
+      if (options.length) setToastActive(true)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [amazonCategory, selectedArray])
 
   const getUnique = (array) => {
     let uniqueArr = [...new Set(array)];
     return uniqueArr
   }
 
-  useEffect(() => {
-    let optionLength = selectedArray.length
-    const tempOptions = []
-    amazonCategory.forEach((item, index) => {
-      if (selectedArray[optionLength - 1].value === item[optionLength - 1] && item[optionLength] !== undefined) {
-        tempOptions.push(item[optionLength])
-      }
-    })
-    if (tempOptions.length) {
-      console.log(tempOptions)
-      let t1 = [...getUnique(tempOptions)]
-      let tempData = []
-      t1.forEach((item, index) => {
-        tempData.push({ label: item, value: item })
-      })
-      setOptions([...options, tempData])
-    } else {
-      if (options.length)
-        setToastActive(true)
-    }
-
-  }, [selectedArray])
-  console.log(amazonCategory);
   const handleSelectValue = (value, index) => {
     var optionSplice = [...options]
     var selectedSplice = [...selectedArray]
-
     if (selectedArray.length > index) {
       optionSplice.splice(index + 1);
       selectedSplice.splice(index + 1);
-      setOptions([...optionSplice])
     }
-
-    selectedSplice[index] = {
-      label: value,
-      value: value
-    }
+    selectedSplice[index] = { label: value, value: value }
     setSelectedArray([...selectedSplice])
+    setOptions([...optionSplice])
   }
-
-  const handleToast = () => {
-    setToastActive(!toastActive)
-  }
-  const toastMarkup = toastActive ? (<Toast
-    message="no more child"
-    onDismiss={() => handleToast()}
-    timeout={3000}
-    type="error"
-  />) : <></>
 
   return (<>
     <FlexLayout halign="center" spacing="tight">
@@ -98,10 +77,12 @@ export default function Home() {
           />
         </FlexChild>)
       })}
-
     </FlexLayout>
-    <ToastWrapper>
-      {toastMarkup}
+    <ToastWrapper>{toastActive &&
+      <Toast message="Has Child : False"
+        onDismiss={() => setToastActive(!toastActive)}
+        timeout={2000} type="error"
+      />}
     </ToastWrapper>
   </>)
 }
